04c70b6cf1b4b2aebd086bd17d917f1c83ba0de1
Francis Pion <francispion@hotmail.com>
2024-10-16 00:33:47 -0400

    Implemented specialization analysis tool.

---
+++ src/TreeHopper/Models/SpecializationPath.cs
@@ -0,0 +1,18 @@
+﻿namespace TreeHopper.Models;
+
+internal record SpecializationPath
+{
+  [JsonIgnore]
+  public Specialization Source { get; }
+  [JsonIgnore]
+  public Specialization Target { get; }
+
+  public bool? IsMandatoryTalentMatch { get; set; }
+  public int OptionalTalentMatches { get; set; }
+
+  public SpecializationPath(Specialization source, Specialization target)
+  {
+    Source = source;
+    Target = target;
+  }
+}
---
+++ src/TreeHopper/Queries/ReadSpecializationsQuery.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using MediatR;
+using TreeHopper.Models;
+
+namespace TreeHopper.Queries;
+
+internal record ReadSpecializationsQuery(string path, Encoding encoding) : IRequest<IReadOnlyCollection<Specialization>>;
+
+internal class ReadSpecializationsQueryHandler : IRequestHandler<ReadSpecializationsQuery, IReadOnlyCollection<Specialization>>
+{
+  public Task<IReadOnlyCollection<Specialization>> Handle(ReadSpecializationsQuery query, CancellationToken cancellationToken)
+  {
+    using StreamReader reader = new(query.path, query.encoding);
+    using CsvReader csv = new(reader, CultureInfo.InvariantCulture);
+    IReadOnlyCollection<Specialization> talents = csv.GetRecords<Specialization>().ToArray().AsReadOnly();
+    return Task.FromResult(talents);
+  }
+}
